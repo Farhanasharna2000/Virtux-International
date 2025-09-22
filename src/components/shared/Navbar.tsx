c4ab7a7f2874807b344383cfa29b8f41a3932da7
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-09-22 13:57:48 +0600

    complete navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,17 +1,47 @@
+import { menuItems } from "@/constants/menu";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Menu } from "lucide-react";
 const Navbar = () => {
   return (
-    <nav className="py-2 flex items-center justify-between">
+    <nav className="py-2  flex items-center justify-between">
       <div className="flex items-center ">
         <Image src="/logo.png" width={70} height={70} alt="" />
-        <div className="font-bold">
+        <Link href="/" className="font-extrabold md:text-2xl ">
           <p>VIRTUX</p>
           <p>INTERNATIONAL</p>
+        </Link>
+      </div>
+      <div className="hidden md:block">
+        <div className="flex items-center justify-between gap-6 md:pr-4 ">
+          {menuItems.map((item) => (
+            <Link key={item.path} href={item.path}>
+              {item.name}
+            </Link>
+          ))}
         </div>
       </div>
-      <div className="flex items-center justify-between"></div>
+      <div className="block md:hidden mx-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Menu />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="mr-4">
+            {menuItems.map((item) => (
+              <Link href={item.path} key={item.path}>
+                <DropdownMenuItem>{item.name}</DropdownMenuItem>
+              </Link>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </nav>
   );
 };
---
+++ src/constants/menu.tsx
@@ -0,0 +1,11 @@
+export const menuItems=[
+    {name:"Home",path:"/"},
+    {name:"About Us",path:"/about"},
+    {name:"Case Study",path:"/case-study"},
+    {name:"Our Team",path:"/our-team"},
+    {name:"Services",path:"/services"},
+    {name:"Portfolio",path:"/portfolio"},
+    {name:"Contact Us",path:"/contact"},
+
+
+]
