e60f86bf38b509caba6cf4d686f1d04ba8987f74
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-09-30 15:21:07 +0600

    Add team page

---
+++ src/app/our-team/page.tsx
@@ -0,0 +1,47 @@
+import { teamMembers } from "@/constants/members";
+import Image from "next/image";
+import React from "react";
+
+const Team: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl md:text-5xl font-bold text-center py-10 md:py-20">
+        Our Team
+      </h1>
+      <div className="py-6 md:py-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-6 md:gap-8">
+          {teamMembers.map((member, index) => (
+            <div
+              key={index}
+              className={`group relative rounded-3xl p-8 cursor-pointer transition-all duration-300 hover:scale-105 ${
+                index < 3 ? "lg:col-span-4" : "lg:col-span-3"
+              }`}
+            >
+              <div className="flex flex-col items-center justify-center text-center h-full space-y-2">
+                <div className="bg-white w-[240px] h-[240px] overflow-hidden rotate-45 rounded-3xl border-2 border-[#208dc7] p-3">
+                  <Image
+                    src={member.image}
+                    alt={member.name}
+                    width={240}
+                    height={240}
+                    className="object-contain w-full h-full -rotate-45 scale-150"
+                  />
+                </div>
+                <div className="mt-16">
+                  <p className="text-[#208dc7] font-bold text-lg">
+                    {member.role}
+                  </p>
+                  <h3 className="text-xl font-bold leading-tight">
+                    {member.name}
+                  </h3>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Team;
---
+++ src/constants/members.tsx
@@ -0,0 +1,99 @@
+import { TeamMember } from "@/types/members";
+
+export const teamMembers: TeamMember[] = [
+    {
+      name: "Rafikul Islam Sohel",
+      role: "Chairman",
+      image: "/Pic/1.png"
+    },
+    {
+      name: "Rafikul Islam (Rakib)",
+      role: "CEO",
+      image: "/Pic/2.png"
+    },
+    {
+      name: "Mazharul Islam Sazid",
+      role: "Director",
+      image: "/Pic/3.png"
+    },
+    {
+      name: "Farhana Akter Sharna",
+      role: "Frontend Developer",
+      image: "/Pic/20.jpg"
+    },
+    {
+      name: "Nosiba Naim",
+      role: "Typography artist",
+      image: "/Pic/4.png"
+    },
+    {
+      name: "Shohidul Ontor",
+      role: "Branding Designer",
+      image: "/Pic/5.png"
+    },
+    {
+      name: "Md. Shazid Ahmed",
+      role: "Visualizer",
+      image: "/Pic/6.png"
+    },
+    {
+      name: "Sahal Sinha",
+      role: "Sports Poster Designer",
+      image: "/Pic/7.png"
+    },
+     {
+      name: "Midul Grafix",
+      role: "Graphic Designer",
+      image: "/Pic/8.png"
+    }, 
+    {
+      name: "Md Salman Khan",
+      role: "Graphic Designer",
+      image: "/Pic/9.png"
+    }, 
+    {
+      name: "Alamin",
+      role: "Graphic Designer",
+      image: "/Pic/10.png"
+    }, 
+    {
+      name: "Kazi Sarid",
+      role: "SEO",
+      image: "/Pic/11.png"
+    },
+     {
+      name: "Midul Grafix",
+      role: "SEO",
+      image: "/Pic/12.png"
+    }, 
+    {
+      name: "Injamul Hoque",
+      role: "Movie Poster Designer",
+      image: "/Pic/13.png"
+    },
+     {
+      name: "Labib Uddin Adil",
+      role: "Digital Marketer",
+      image: "/Pic/14.png"
+    },
+    {
+      name: "Kazi Sarid",
+      role: "SEO",
+      image: "/Pic/15.png"
+    },
+     {
+      name: "Md Zunayed Khan",
+      role: "Digital Marketing",
+      image: "/Pic/16.png"
+    },
+    {
+      name: "Jahadul Islam",
+      role: "Video Editor",
+      image: "/Pic/17.png"
+    },
+    {
+      name: "Sadia Karim",
+      role: "SEO",
+      image: "/Pic/18.png"
+    },
+  ];
